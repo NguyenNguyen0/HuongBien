a2c981e080e0493b8482ae8af205f8f508b5268b
nvminh162 <nvminh162@gmail.com>
2024-10-18 14:59:03 +0700

    update: login connect with database

--- HuongBienRestaurant/src/main/java/com/huongbien/ui/controller/GUI_LoginController.java
+++ HuongBienRestaurant/src/main/java/com/huongbien/ui/controller/GUI_LoginController.java
@@ -1,5 +1,9 @@
 package com.huongbien.ui.controller;
 
+import com.huongbien.dao.Account_DAO;
+import com.huongbien.database.Database;
+import com.huongbien.entity.Account;
+import com.huongbien.utils.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,10 +11,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -19,10 +20,15 @@ import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Optional;
 import java.util.ResourceBundle;
+
+import static com.huongbien.database.Database.connection;
 
 public class GUI_LoginController implements Initializable {
     @FXML
@@ -48,6 +54,12 @@ public class GUI_LoginController implements Initializable {
 
     @FXML
     private ImageView togglePwdBtn;
+
+    @FXML
+    private Label text_message;
+
+    //DAO
+    private Account_DAO accountDao;
 
     private void loadCarousel() {
         try {
@@ -87,43 +99,85 @@ public class GUI_LoginController implements Initializable {
 
     @FXML
     void togglePwdBtn(MouseEvent event) {
-        if(!status) {
+        if (!status) {
             Image imgShow = new Image(getClass().getResourceAsStream("/com/huongbien/icon/mg_login/eye-16px.png"));
             togglePwdBtn.setImage(imgShow);
             compoent_show.setVisible(true);
+            txt_pwdShow.setVisible(true);
             compoent_hide.setVisible(false);
-            txt_pwdShow.setText(txt_pwdHide.getText()+"");
+            txt_pwdHide.setVisible(false);
+            txt_pwdShow.setText(txt_pwdHide.getText() + "");
             status = true;
         } else {
             Image imgHide = new Image(getClass().getResourceAsStream("/com/huongbien/icon/mg_login/hiddenEye-16px.png"));
             togglePwdBtn.setImage(imgHide);
             compoent_show.setVisible(false);
+            txt_pwdShow.setVisible(false);
             compoent_hide.setVisible(true);
-            txt_pwdHide.setText(txt_pwdShow.getText()+"");
+            txt_pwdHide.setVisible(true);
+            txt_pwdHide.setText(txt_pwdShow.getText() + "");
             status = false;
         }
+    }
+
+    private String getCurrentPwd() {
+        if (txt_pwdHide.isVisible()) {
+            return txt_pwdHide.getText();
+        }
+        if (txt_pwdShow.isVisible()) {
+            return txt_pwdShow.getText();
+        }
+        return "";
     }
 
     @FXML
     void handleLogin(ActionEvent event) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/huongbien/fxml/GUI_Main.fxml"));
-            Parent root = loader.load();
+            Connection connection = Database.getConnection();
+            accountDao = new Account_DAO(connection);
+            //
+            String username = txt_empID.getText();
+            String password = getCurrentPwd();
+            String passwordHash = Utils.hashPassword(password);
 
-            Scene mainScene = new Scene(root);
+            Account account = accountDao.get(username);
 
-            Stage loginStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-            loginStage.close();
+            if (account == null) {
+                text_message.setText("Tài khoản không tồn tại.");
+                text_message.setStyle("-fx-text-fill: red;");
+            }
+//            text_message.setText(passwordHash);
+            if (passwordHash.equals(account.getHashcode()) && username.equals(account.getUsername())) {
+                text_message.setText("Đăng nhập thành công");
+                text_message.setStyle("-fx-text-fill: green;");
 
-            Stage mainStage = new Stage();
-            mainStage.setScene(mainScene);
-            mainStage.setMaximized(true);
-            mainStage.setTitle("Dashboard - Huong Bien Restaurant");
-            mainStage.initStyle(StageStyle.UNDECORATED);
-            mainStage.show();
-
-        } catch (Exception e) {
-            e.printStackTrace();
+                //load main
+                try {
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/huongbien/fxml/GUI_Main.fxml"));
+                    Parent root = loader.load();
+                    //
+                    Scene mainScene = new Scene(root);
+                    //
+                    Stage loginStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                    loginStage.close();
+                    //
+                    Stage mainStage = new Stage();
+                    mainStage.setScene(mainScene);
+                    mainStage.setMaximized(true);
+                    mainStage.setTitle("Dashboard - Huong Bien Restaurant");
+                    mainStage.initStyle(StageStyle.UNDECORATED);
+                    mainStage.show();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else {
+                text_message.setText("Sai mã nhân viên và mật khẩu đăng nhập");
+                text_message.setStyle("-fx-text-fill: red;");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            Database.closeConnection();
         }
     }
 
