f1336c7429e94275a7516aafa1d14fcc557fb92b
nguyennguyen0 <trungnguyenwork123@gmail.com>
2024-11-18 11:45:57 +0700

    feat: add Constants class for file path configurations in the application, remove old code in LookupController

---
+++ HuongBienRestaurant/src/main/java/com/huongbien/config/Constants.java
@@ -0,0 +1,9 @@
+package com.huongbien.config;
+
+public class Constants {
+    public final static String LOGIN_SESSION_PATH = "src/main/resources/com/huongbien/temp/loginSession.json";
+    public final static String TEMPORARY_CUISINE_PATH = "src/main/resources/com/huongbien/temp/temporaryCuisine.json";
+    public final static String TEMPORARY_TABLE_PATH = "src/main/resources/com/huongbien/temp/temporaryTable.json";
+    public final static String TEMPORARY_CUSTOMER_PATH = "src/main/resources/com/huongbien/temp/temporaryCustomer.json";
+    public final static String PAYMENT_QUEUE_PATH = "src/main/resources/com/huongbien/temp/paymentQueue.json";
+}
